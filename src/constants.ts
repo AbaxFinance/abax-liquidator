30ae31ee04310981222a65461ce226d6c90946fb
WookashWackomy <wookie.xp.07@gmail.com>
2023-12-06 14:08:08 +0100

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -3,24 +3,30 @@ export const ONE_SECOND = 1000;
 export const ONE_MINUTE = ONE_SECOND * 60;
 export enum HF_PRIORITY {
   CRITICAL = 0,
-  HIGH = 1,
-  MEDIUM = 2,
-  LOW = 3,
-  SAFE = 4,
+  VERY_HIGH = 1,
+  HIGH = 2,
+  MEDIUM = 3,
+  LOW = 4,
+  SAFE = 5,
+  SAFEST = 6,
 }
 
 export const UPDATE_INTERVAL_BY_HF_PRIORITY = {
-  [HF_PRIORITY.CRITICAL]: 30 * ONE_SECOND,
-  [HF_PRIORITY.HIGH]: 3 * ONE_MINUTE,
+  [HF_PRIORITY.CRITICAL]: 10 * ONE_SECOND,
+  [HF_PRIORITY.VERY_HIGH]: 30 * ONE_SECOND,
+  [HF_PRIORITY.HIGH]: 1 * ONE_MINUTE,
   [HF_PRIORITY.MEDIUM]: 5 * ONE_MINUTE,
-  [HF_PRIORITY.LOW]: 15 * ONE_MINUTE,
-  [HF_PRIORITY.SAFE]: 30 * ONE_MINUTE,
-};
+  [HF_PRIORITY.LOW]: 10 * ONE_MINUTE,
+  [HF_PRIORITY.SAFE]: 15 * ONE_MINUTE,
+  [HF_PRIORITY.SAFEST]: 30 * ONE_MINUTE,
+} satisfies Record<HF_PRIORITY, number>;
 
 export const HF_RANGE_BY_HF_PRIORITY = {
   [HF_PRIORITY.CRITICAL]: [0.0, 1.05] as const,
-  [HF_PRIORITY.HIGH]: [1.05, 1.2] as const,
-  [HF_PRIORITY.MEDIUM]: [1.2, 1.5] as const,
-  [HF_PRIORITY.LOW]: [1.5, 2.0] as const,
-  [HF_PRIORITY.SAFE]: [2.0, Number.MAX_SAFE_INTEGER] as const,
-};
+  [HF_PRIORITY.VERY_HIGH]: [1.05, 1.1] as const,
+  [HF_PRIORITY.HIGH]: [1.1, 1.5] as const,
+  [HF_PRIORITY.MEDIUM]: [1.5, 1.8] as const,
+  [HF_PRIORITY.LOW]: [1.8, 2.5] as const,
+  [HF_PRIORITY.SAFE]: [2.5, 3.5] as const,
+  [HF_PRIORITY.SAFEST]: [3.5, Number.MAX_SAFE_INTEGER] as const,
+} satisfies Record<HF_PRIORITY, [number, number]>;
